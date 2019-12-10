8157cc55e1aa699dc5b41f9af2e078e967dafdcc
Yashar Mateen <yasheymateen@gmail.com>
2019-12-10 07:24:25 +0000

    modifed 1st task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -5,5 +5,6 @@ file { '/tmp/holberton':
   path     => '/tmp/holberton',
   mode     => '0744',
   owner    => 'www-data',
-  group    => 'I love Puppet'
+  group    => 'www-data',
+  content    => 'I love Puppet'
 }
