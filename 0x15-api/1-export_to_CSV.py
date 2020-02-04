1816531a5ba4a190f55a6e64a50305ab95bd3d34
yasheymateen <yasheymateen@gmail.com>
2020-02-04 04:33:39 +0000

    modify 1 file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ extend python script to export data in the csv format """
 
-import json
-import sys
 import csv
 import requests
+import sys
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -13,11 +12,11 @@ if __name__ == '__main__':
     user_id = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/todos?userId=' + user_id
     r = requests.get(url)
-    file_name = user_id + ".csv"
+    filename = user_id + ".csv"
     n = requests.get('https://jsonplaceholder.typicode.com/users/' + user_id)
     name = n.json().get('username')
 
-    with open(file_name, 'w', newline='') as csvfile:
+    with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
         for user in r.json():
             writer.writerow([user_id, name, str(user.get('completed')),
