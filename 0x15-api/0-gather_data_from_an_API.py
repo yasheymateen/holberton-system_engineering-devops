4be4b13f60d7e4735d1a8a40dfe494f28f7cf932
yasheymateen <yasheymateen@gmail.com>
2020-02-04 03:26:23 +0000

    adding readme and first task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" using rest API, return information about a given employee id's progress"""
+
+import json
+import sys
+import requests
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        exit(1)
+
+    name = 'https://jsonplaceholder.typicode.com/users' + sys.argv[1]
+    name = requests.get(name).json().get('name')
+    url = 'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
+    r = requests.get(url)
+
+    total_tasks = 0
+    number_done = 0
+    task_progress = []
+    for user in r.json():
+        total_tasks += 1
+        if user.get('completed') is True:
+            task_progress.append(user.get('title'))
+            number_done += 1
+    first_line = 'Employee {} is done with tasks({}/{})'.format(name,
+                                                                number_done,
+                                                                total_tasks)
+    print(first_line)
+    for task in task_progress:
+        print('\t', task)
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+## This project dives into using an API to access employee data to organize and export them to different data structures
