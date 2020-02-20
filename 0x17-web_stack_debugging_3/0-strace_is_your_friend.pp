b46c2e320207c2110a2cd1ff2bb931e8b5aa1eaa
yasheymateen <yasheymateen@gmail.com>
2020-02-20 05:33:28 +0000

    modified first file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fixes type in wp-settings.php where extension is mispelled
-exec { 'fix typo':
-     command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-     path    => ['/bin/']
-}
+exec { 'mv class-wp-locale.php class-wp-locale.phpp':
+  path => ['/bin'],
+  cwd  => '/var/www/html/wp-includes',
+
+  }
